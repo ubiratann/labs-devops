b3bfb2d10fbbec475c65b70370d6968b35dc7288
Ubiratan <r14.ubiratanjunior@gmail.com>
2022-02-26 04:58:10 -0300

    Adiciona permissao de pasta para importar roles

--- Cap03/control-node/provision.sh
+++ Cap03/control-node/provision.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sudo apt install ansible -y
+sudo chmod +w+r+x -R /etc/ansible #garantindo permissão para importar roles do ansible-galaxy
 cat << EOT >> /etc/hosts
 192.167.1.2 control-node
 192.167.1.3 app01
