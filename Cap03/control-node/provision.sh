8734e0d65b5e4c1ee78ca60185ece8a336005523
Ubiratan <r14.ubiratanjunior@gmail.com>
2022-02-26 06:32:17 -0300

    Atualizando repositorio de instalação do ansible

--- Cap03/control-node/provision.sh
+++ Cap03/control-node/provision.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+#instalando versao atualizada do ansible
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt install ansible -y
-sudo chmod +w+r+x -R /etc/ansible #garantindo permissão para importar roles do ansible-galaxy
+
+#garantindo permissão para importar roles do ansible-galaxy
+sudo chmod +w+r+x -R /etc/ansible 
+
+#editando arquivo de hosts
 cat << EOT >> /etc/hosts
 192.167.1.2 control-node
 192.167.1.3 app01
