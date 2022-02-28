24ceef605845cc3091ee7bdbf4381f0780016be7
Ubiratan <r14.ubiratanjunior@gmail.com>
2022-02-28 15:50:49 -0300

    Atualizando provision para gerar chave publica

--- Cap03/control-node/provision.sh
+++ Cap03/control-node/provision.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 #instalando versao atualizada do ansible
-sudo apt update -y && sudo apt upgrade -y
 sudo apt install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt install ansible -y
@@ -9,9 +8,17 @@ sudo apt install ansible -y
 #garantindo permissão para importar roles do ansible-galaxy
 sudo chmod +w+r+x -R /etc/ansible 
 
+echo "Adicionando hosts"
 #editando arquivo de hosts
 cat << EOT >> /etc/hosts
 192.167.1.2 control-node
 192.167.1.3 app01
 192.167.1.4 db01
 EOT
+
+echo "Gerando chave publica"
+#gerando chave compartilhada
+ssh-keygen -t rsa -N '' -f /home/vagrant/.ssh/id_rsa -C "vagrant@control-node"
+chmod -R +rw /home/vagrant/.ssh
+mkdir -p /vagrant/ssh
+cp /home/vagrant/.ssh/id_rsa.pub /vagrant/ssh
